376241c170c08651499efb23101bc525f4fcbae0
Jean Gustavo <jean.gustavo@intergalaxy.io>
2021-11-30 16:35:55 -0300

    Created AccountController -> getBalance() and BankController -> reset()

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Account;
 use Illuminate\Http\Request;
 
 class AccountController extends Controller
 {
     public function getBalance(Request $request){
-        return response()->json(true, 200);
+        $account = Account::find($request->account_id);
+
+        if(!$account) return response()->json(0, 404);
+
+        return response()->json($account->balance, 200);
     }
 }
--- app/Http/Controllers/BankController.php
+++ app/Http/Controllers/BankController.php
@@ -3,11 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 class BankController extends Controller
 {
     public function reset()
     {
-        return response()->json(true, 200);
+        $tables = DB::select('SHOW TABLES');
+        Schema::disableForeignKeyConstraints();
+        foreach ($tables as $table) {
+            foreach($table as $table_name){
+                if ($table_name == 'migrations') {
+                    continue;
+                }
+                DB::table($table_name)->truncate();
+            }
+        }
+        Schema::enableForeignKeyConstraints();
+
+        return response('OK', 200);
     }
 }
