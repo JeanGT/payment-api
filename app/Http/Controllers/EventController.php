6fd56590f1089d7ac59b4ee00c2264787682c2c7
Jean Gustavo <jean.gustavo@intergalaxy.io>
2021-11-30 16:39:11 -0300

    created EventController -> handle() basic structure

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -8,6 +8,27 @@ use Illuminate\Http\Request;
 class EventController extends Controller
 {
     public function handle(Request $request){        
+        switch($request->type){
+            case 'deposit':
+                return $this->deposit($request);
+            case 'withdraw':
+                return $this->withdraw($request);
+            case 'transfer':
+                return $this->transfer($request);
+            default:
+                //throw an exception
+        }
+    }
+
+    private function deposit(Request $request){
+        return response()->json(true, 200);
+    }
+
+    private function withdraw(Request $request){
+        return response()->json(true, 200);
+    }
+
+    private function transfer(Request $request){
         return response()->json(true, 200);
     }
 }
